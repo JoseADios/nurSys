2502063ffdc8ffb1553554dff81bff65ef29df05
joseDios <josedios10@gmail.com>
2024-12-10 10:39:45 -0400

    Medical_orders table

---
+++ app/Http/Controllers/MedicalOrderController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medical_order;
+use Illuminate\Http\Request;
+
+class MedicalOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medical_order $medical_order)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Medical_order $medical_order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medical_order $medical_order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medical_order $medical_order)
+    {
+        //
+    }
+}

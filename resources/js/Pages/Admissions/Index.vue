676a202eddf14d1d4c37ab2c0d6358e2a795952a
joseDios <josedios10@gmail.com>
2024-12-11 13:57:08 -0400

    Patient first_name

--- resources/js/Pages/Admissions/Index.vue
+++ resources/js/Pages/Admissions/Index.vue
@@ -29,7 +29,7 @@
                     <tr v-for="admission in admissions" :key="admission.id"
                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ admission.patient.name }}
+                            {{ admission.patient.first_name }}
                         </th>
                         <td class="px-6 py-4">
                             {{ admission.bed.number }}

82d8e54071ec80626715ec29dee7a6e607565e84
Eudis Contreras <150478715+EudisContreras@users.noreply.github.com>
2024-12-27 21:59:08 -0400

    Update Show.vue

--- resources/js/Pages/MedicationRecords/Show.vue
+++ resources/js/Pages/MedicationRecords/Show.vue
@@ -78,7 +78,7 @@
                             class="w-full bg-blue-600 text-white py-2 px-4 rounded-md
                                 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
                                 transition-colors duration-300">
-                            Agregar Detalles
+                            Agregar Detalle
                         </Link>
                     </div>
                 </div>

e02b5570258bad05a4889be96b0f8b7eaa2481e5
EudisContreras <eudiscontreras1329@gmail.com>
2025-01-11 19:55:30 -0400

    edit migrations medical order2

--- database/migrations/2024_12_11_183234_create_marital_statuses_table.php
+++ database/migrations/2024_12_11_183234_create_marital_statuses_table.php
@@ -9,6 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    //
     public function up(): void
     {
         Schema::create('marital_statuses', function (Blueprint $table) {

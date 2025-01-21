5d263f2b74da3db92eb9da872e8eebe5409ff406
EudisContreras <eudiscontreras1329@gmail.com>
2025-01-21 13:48:55 -0400

    fixed  diet

--- app/Http/Controllers/MedicationRecordDetailController.php
+++ app/Http/Controllers/MedicationRecordDetailController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\MedicationRecord;
 use App\Models\MedicationNotification;
 use App\Models\MedicationRecordDetail;
+use App\Models\Drug;
+use App\Models\DrugDose;
+use App\Models\DrugRoute;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+
 
 class MedicationRecordDetailController extends Controller
 {
@@ -27,9 +32,18 @@ class MedicationRecordDetailController extends Controller
     public function create($medicationRecordId)
     {
         $medicationRecord = MedicationRecord::findOrFail($medicationRecordId);
+
+        $drug = Drug::all();
+        $dose = DrugDose::all();
+        $route = DrugRoute::all();
+
+
         return Inertia::render('MedicationRecordDetail/Create', [
             'medicationRecord' => $medicationRecord,
-            'id' => $medicationRecord->id
+            'id' => $medicationRecord->id,
+            'drug' => $drug,
+            'dose' => $dose,
+            'routeOptions' => $route
         ]);
     }
 
@@ -67,9 +81,8 @@ class MedicationRecordDetailController extends Controller
                 MedicationNotification::create([
                     'medication_record_detail_id' => $detail->id, // Usa el ID del detalle
                     'scheduled_time' => $scheduled_time,
-                    'nurse_sign' => "a",
                     "active"=> 1,
-                    'nurse_id' => 1,
+                    "nurse_id" => Auth::id(),
 
 
                 ]);
@@ -109,107 +122,112 @@ class MedicationRecordDetailController extends Controller
         $fc = $request->fc;
         $interval_in_hours = $request->interval_in_hours;
         $start_time = $request->start_time;
-
-
         $notifications = $medicationRecordDetail->medicationNotification()->count();
         $lastNotification = $medicationRecordDetail
-            ->medicationNotification()
-            ->latest('scheduled_time')
-            ->first();
+        ->medicationNotification()
+        ->latest('scheduled_time')
+        ->first();
 
-        $lastNotificationform = $lastNotification ? Carbon::parse($lastNotification->scheduled_time) : null;
+$lastNotificationform = $lastNotification ? Carbon::parse($lastNotification->scheduled_time) : null;
+
+    // Validar la frecuencia al saber el nuevo valor si hay mas notificaciones eliminar las que sobran y si faltan crearlas.
+    if($fc > $notifications){
+
+        $notifications_add = $fc - $notifications;
+
+        for ($i = 1; $i < $notifications_add; $i++) {
+
+
+            $scheduled_time = $lastNotificationform->copy()->addHours($i * $request->interval_in_hours);
+
+          try {
+            $medicationRecordDetail->medicationNotification()->create([
+                'medication_record_detail_id' => $medicationRecordDetail->id,
+                'scheduled_time' => $scheduled_time,
+
+                "active"=> 1,
+                "nurse_id" => Auth::id(),
+
+
+            ]);
+          } catch (\Throwable $th) {
+            Log::error('error en crear'.$th->getMessage());
+          }
+
+        }
+
+    } elseif ($fc < $notifications) {
+
+        try {
+            $notifications_delete = $notifications - $fc;
+            $medicationRecordDetail->medicationNotification()->latest()->take($notifications_delete)->get()->each(function($notification){
+                $notification->delete();
+            });
+        } catch (\Throwable $th) {
+            Log::error('error en eliminar'.$th->getMessage());
+          }
+
+    }
+    else{
+      Log::error('no hubo cambios delete/create');
+    }
+
+
+
+
+
+
+
 
 
         // Si intervalo en horas se cambia actualizar hora programada para todas las notificaciones relacionadas.
-        if ($medicationRecordDetail->interval_in_hours != $interval_in_hours) {
-
-
-           try{
-            $notificationsi = $medicationRecordDetail->medicationNotification()->get();
-            foreach ($notificationsi as $index => $notification) {
-                $scheduled_time = $lastNotificationform
-                    ->copy()
-                    ->addHours($index * $interval_in_hours)
-                    ->toDateTimeString();
-
-                $notification->update(['scheduled_time' => $scheduled_time]);
-            }
-
-           }  catch (\Throwable $th) {
-                Log::error('error en interval'.$th->getMessage());
-              }
-
-
-        }
-
-
-          // Si se cambia hora de inicio actualizar hora programada para todas las notificaciones relacionadas.
-        if ($medicationRecordDetail->start_time != $start_time) {
-
-
-            try{
-
-                $start_time = Carbon::parse($start_time);
-
-                $notificationsi = $medicationRecordDetail->medicationNotification()->get();
-                foreach ($notificationsi as $index => $notification) {
-
-
-                    $scheduled_time = $start_time
-                        ->copy()
-                        ->addHours($index * $interval_in_hours)
-                        ->toDateTimeString();
-
-                    $notification->update(['scheduled_time' => $scheduled_time]);
-                }
-
-               }  catch (\Throwable $th) {
-                    Log::error('error en interval'.$th->getMessage());
-                  }
-        }
-
-
-        //  // Validar la frecuencia al saber el nuevo valor si hay mas notificaciones eliminar las que sobran y si faltan crearlas.
-        if($fc > $notifications){
-
-            $notifications_add = $fc - $notifications;
-
-            for ($i = 0; $i < $notifications_add; $i++) {
-
-
-                $scheduled_time = $lastNotificationform->copy()->addHours($i * $request->interval_in_hours);
-
-              try {
-                $medicationRecordDetail->medicationNotification()->create([
-                    'medication_record_detail_id' => $medicationRecordDetail->id,
-                    'scheduled_time' => $scheduled_time,
-                    'nurse_sign' => "a",
-                    "active"=> 1,
-                    'nurse_id' => 1,
-
-
-                ]);
-              } catch (\Throwable $th) {
-                Log::error('error en crear'.$th->getMessage());
-              }
-
-            }
-
-        } elseif ($fc < $notifications) {
-
+        if ($medicationRecordDetail->interval_in_hours != $interval_in_hours || $medicationRecordDetail->start_time != $start_time) {
             try {
-                $notifications_delete = $notifications - $fc;
-                $medicationRecordDetail->medicationNotification()->latest()->take($notifications_delete)->get()->each(function($notification){
-                    $notification->delete();
-                });
-            } catch (\Throwable $th) {
-                Log::error('error en eliminar'.$th->getMessage());
-              }
+                $notificationsi = $medicationRecordDetail->medicationNotification()->get();
 
-        }
-        else{
-          Log::error('no hubo cambios');
-        }
+                if ($medicationRecordDetail->interval_in_hours != $interval_in_hours && $medicationRecordDetail->start_time == $start_time) {
+                        // Actualizar solo en base al intervalo en horas
+                    $formatted_start_time = Carbon::parse($start_time);
+                    foreach ($notificationsi as $index => $notification) {
+                        $scheduled_time = $formatted_start_time
+                            ->copy()
+                            ->addHours($index * $interval_in_hours)
+                            ->toDateTimeString();
+
+                        $notification->update(['scheduled_time' => $scheduled_time]);
+                    }
+                } elseif ($medicationRecordDetail->start_time != $start_time && $medicationRecordDetail->interval_in_hours == $interval_in_hours) {
+                    // Actualizar solo en base a la hora de inicio
+                    $formatted_start_time = Carbon::parse($start_time);
+                    foreach ($notificationsi as $index => $notification) {
+                        $scheduled_time = $formatted_start_time
+                            ->copy()
+                            ->addHours($index * $interval_in_hours)
+                            ->toDateTimeString();
+
+                        $notification->update(['scheduled_time' => $scheduled_time]);
+                    }
+                } elseif ($medicationRecordDetail->interval_in_hours != $interval_in_hours && $medicationRecordDetail->start_time != $start_time) {
+                    // Ambas variables cambiaron, aplicar actualizaciones y registrar en el log
+                    $formatted_start_time = Carbon::parse($start_time);
+                    foreach ($notificationsi as $index => $notification) {
+                        $scheduled_time = $formatted_start_time
+                            ->copy()
+                            ->addHours($index * $interval_in_hours)
+                            ->toDateTimeString();
+
+                        $notification->update(['scheduled_time' => $scheduled_time]);
+                    }
+                }
+            } catch (\Throwable $th) {
+                Log::error('Error al actualizar notificaciones: ' . $th->getMessage());
+            }
+        }else{
+            Log::error('no hubo cambios');
+          }
+          // Si se cambia hora de inicio actualizar hora programada para todas las notificaciones relacionadas.
+
+
 
 
         $medicationRecordDetail->update($request->all());

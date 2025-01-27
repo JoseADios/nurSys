bb7b49cab40ed238a2199feee1faea5990a6f775
EudisContreras <eudiscontreras1329@gmail.com>
2025-01-26 20:50:16 -0400

    fixed  create route for medication records

--- resources/js/Pages/MedicationRecords/Show.vue
+++ resources/js/Pages/MedicationRecords/Show.vue
@@ -79,7 +79,7 @@
                 <div class="p-8">
                     <h3 class="text-xl font-semibold text-gray-800 dark:text-white mb-6">Agregar Nuevo Detalle</h3>
                     <div>
-                        <Link :href="route('create',medicationRecord.id)"
+                        <Link :href="route('medicationRecordDetails.create',medicationRecord.id)"
                             class="w-full bg-blue-600 text-white py-2 px-4 rounded-md
                                 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
                                 transition-colors duration-300">

b323b96c168318a9cbf78c90633503368e9efb2b
joseDios <josedios10@gmail.com>
2025-01-27 21:20:36 -0400

    Fix show deleted temperature records

--- resources/js/Pages/TemperatureRecords/Index.vue
+++ resources/js/Pages/TemperatureRecords/Index.vue
@@ -7,10 +7,13 @@
         </template>
 
         <!-- Filtro para mostrar registros eliminados -->
-        <div class="p-4 bg-gray-100 dark:bg-gray-900 flex justify-between items-center">
-            <label class="flex items-center space-x-2 text-blue-600 hover:text-blue-800 transition-colors">
-                <input type="checkbox" v-model="showDeleted" @change="toggleShowDeleted">
-                <span class="font-medium">Mostrar registros eliminados</span>
+        <div class="p-4 flex justify-end items-center">
+            <label class="flex items-center space-x-2 text-white hover:text-red-300 transition-colors">
+            <input type="checkbox" v-model="showDeleted" :checked="showDeleted" :true-value="1" :false-value="0" @change="toggleShowDeleted">
+            <span class="font-medium">Ver eliminados</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.707a1 1 0 00-1.414-1.414L10 8.586 7.707 6.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z" clip-rule="evenodd" />
+            </svg>
             </label>
         </div>
 

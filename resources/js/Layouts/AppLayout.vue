f31060da63b60c1359e27d38c8226f4e31a79182
joseDios <josedios10@gmail.com>
2025-03-13 13:53:18 -0400

    Add dark mode in responsive menu

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -160,8 +160,7 @@ const logout = () => {
                     </div>
 
                     <div class="px-3">
-                        <Tooltip class="w-full" text="Registros de Enfermería"
-                            position="right">
+                        <Tooltip class="w-full" text="Registros de Enfermería" position="right">
                             <Link :href="route('nurseRecords.index')" :class="[
                                 route().current('nurseRecords.index') ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white' : 'text-gray-600 hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-gray-700',
                                 'group flex items-center px-3 py-3 text-sm font-medium rounded-md transition-all',
@@ -360,6 +359,14 @@ const logout = () => {
                                     API Tokens
                                 </ResponsiveNavLink>
 
+                                <!-- boton de modo oscuro -->
+                                <button @click="toggleDarkMode" class="ml-4 text-gray-500 dark:text-gray-400 flex items-center">
+                                    <span class="mr-4">Modo oscuro</span>
+                                    <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"
+                                        :class="{ 'after:translate-x-full peer-checked:after:translate-x-full after:ring-purple-300 ': isDarkMode }">
+                                    </div>
+                                </button>
+
                                 <!-- Authentication -->
                                 <form method="POST" @submit.prevent="logout">
                                     <ResponsiveNavLink as="button">
@@ -436,12 +443,12 @@ const logout = () => {
                     <!-- Perfil en la esquina superior derecha cuando el sidebar está expandido -->
                     <div class="hidden sm:flex sm:items-center sm:ms-6">
                         <!-- Botón de modo oscuro -->
-                        <button
-                            @click="toggleDarkMode"
-                            class="p-1.5 mr-8 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 rounded-full focus:outline-none"
-                        >
-                            <SunIcon v-if="isDarkMode" class="h-5 w-5 text-yellow-400 dark:text-yellow-300 hover:text-yellow-500 dark:hover:text-yellow-400" />
-                            <MoonIcon v-else class="h-5 w-5 text-indigo-600 dark:text-indigo-300 hover:text-indigo-500 dark:hover:text-indigo-400" />
+                        <button @click="toggleDarkMode"
+                            class="p-1.5 mr-8 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 rounded-full focus:outline-none">
+                            <SunIcon v-if="isDarkMode"
+                                class="h-5 w-5 text-yellow-400 dark:text-yellow-300 hover:text-yellow-500 dark:hover:text-yellow-400" />
+                            <MoonIcon v-else
+                                class="h-5 w-5 text-indigo-600 dark:text-indigo-300 hover:text-indigo-500 dark:hover:text-indigo-400" />
                         </button>
 
                         <!-- Teams Dropdown -->

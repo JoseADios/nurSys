69e5ac862901797f3723b305549a8059a62722bc
joseDios <josedios10@gmail.com>
2025-04-17 22:32:54 -0400

    Fix space in patient index filters section

--- resources/js/Pages/Patients/Index.vue
+++ resources/js/Pages/Patients/Index.vue
@@ -67,7 +67,7 @@
         </div>
 
         <!-- Barra de búsqueda y acciones -->
-        <div class="bg-gray-100 dark:bg-gray-900 px-4 lg:px-10 mt-4">
+        <div class="bg-gray-100 dark:bg-gray-900 px-4 lg:px-10 mt-2">
             <div class="flex flex-col md:flex-row gap-4 py-2">
                 <!-- Búsqueda -->
                 <div class="relative w-full md:w-1/3">
@@ -133,7 +133,7 @@
         </div>
 
         <!-- Tabla de pacientes -->
-        <div class="px-4 lg:px-10 mt-6 mb-6">
+        <div class="px-4 lg:px-10 mt-2 mb-6">
             <div class="relative overflow-x-auto border border-gray-200 dark:border-gray-700/60 rounded-t-lg">
                 <div class="overflow-x-auto">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">

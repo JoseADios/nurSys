aacc0c47a3a0e3c20ef4cd1ec11c873ab88892da
EudisContreras <eudiscontreras1329@gmail.com>
2025-05-12 21:08:41 -0400

    added * on required fields for forms

--- resources/js/Pages/MedicationRecords/Create.vue
+++ resources/js/Pages/MedicationRecords/Create.vue
@@ -35,7 +35,7 @@
                 <!-- Selector -->
                 <div class="flex-1">
                     <label for="diet-select" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Dieta
+                        Dieta  <span class="text-red-500">*</span>
                     </label>
                     <select id="diet-select" v-model="form.diet" required class="text-sm font-medium w-full text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100  p-2.5 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                         <option v-for="diets in diet" :key="diets.id" :value="diets.name">

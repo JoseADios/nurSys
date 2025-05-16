1890faa41eff0d538e4fc879ac20338b72ca4f83
joseDios <josedios10@gmail.com>
2025-05-15 22:16:46 -0400

    Make Link responsive

--- resources/js/Components/PrimaryLink.vue
+++ resources/js/Components/PrimaryLink.vue
@@ -47,7 +47,7 @@ defineProps({
         :replace="replace"
         :only="only"
         :data="data"
-        class="inline-flex items-center px-4 py-2 bg-primary-500 dark:bg-primary-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-primary-700 dark:hover:bg-primary focus:bg-primary-700 dark:focus:bg-primary active:bg-primary-900 dark:active:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150"
+        class="inline-flex w-full items-center justify-center px-4 py-2 bg-primary-500 dark:bg-primary-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-primary-700 dark:hover:bg-primary focus:bg-primary-700 dark:focus:bg-primary active:bg-primary-900 dark:active:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150"
     >
         <slot />
     </Link>

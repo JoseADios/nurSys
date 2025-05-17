9e7553056208f88da8220e6f6b9c9890f88e6853
EudisContreras <eudiscontreras1329@gmail.com>
2025-05-16 21:12:16 -0400

    added form processing on admission create and edit

--- resources/js/Pages/Admissions/Edit.vue
+++ resources/js/Pages/Admissions/Edit.vue
@@ -105,7 +105,7 @@
                 Cancelar
                 </Link>
 
-                <button type="submit" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Guardar</button>
+                <button type="submit"  :class="{ 'opacity-25': form.processing }" :disabled="form.processing"  class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Guardar</button>
             </div>
         </form>
     </div>
@@ -185,7 +185,7 @@ export default {
     },
     methods: {
         submit() {
-            this.$inertia.put(route('admissions.update', this.admission.id), this.form, {
+            this.form.put(route('admissions.update', this.admission.id), {
                 onError: (errors) => {
                     this.form.errors = errors;
                 }

ed0cd2b00aa7a4dd46a479546ee76175a855670c
EudisContreras <eudiscontreras1329@gmail.com>
2025-05-19 20:45:30 -0400

    Added Custom  Icons on admission show

--- resources/js/Pages/Admissions/Show.vue
+++ resources/js/Pages/Admissions/Show.vue
@@ -148,12 +148,7 @@
                             <div class="flex flex-col space-y-2 items-center">
                                 <Link :href="route('medicalOrders.index', { admission_id: admission.id })"
                                     class="flex w-full items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold rounded-lg p-4 hover:from-blue-600 hover:to-blue-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
-                                    </path>
-                                </svg>
+                                <MedicalOrderIcon class="size-5 mr-1" />
                                 Órdenes Médicas
                                 </Link>
                                 <Link v-if="can.createOrder"
@@ -166,12 +161,7 @@
                             <div class="flex flex-col space-y-2 items-center">
                                 <Link :href="route('nurseRecords.index', { admission_id: admission.id })"
                                     class="flex w-full items-center justify-center bg-gradient-to-r from-green-500 to-green-700 text-white font-semibold rounded-lg p-4 hover:from-green-600 hover:to-green-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z">
-                                    </path>
-                                </svg>
+                                <NurseRecordIcon class="size-5 mr-1" />
                                 Hojas de Enfermería
                                 </Link>
                                 <AccessGate :role="['admin', 'nurse']">
@@ -187,22 +177,14 @@
                                     <Link
                                         :href="`${route('temperatureRecords.show', temperatureRecordId)}?admission_id=${admission.id}`"
                                         class="flex w-full items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 text-white font-semibold rounded-lg p-4 hover:from-purple-600 hover:to-purple-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
+                                    <ChartIcon class="size-5 mr-1" />
                                     Hoja de Temperaturas
                                     </Link>
                                 </div>
                                 <div v-else class=" w-full">
                                     <Link :href="route('temperatureRecords.create', { admission_id: admission.id })"
                                         class="flex w-full items-center justify-center bg-gradient-to-r from-purple-500 to-purple-700 text-white font-semibold rounded-lg p-4 hover:from-purple-600 hover:to-purple-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
+                                    <ChartIcon class="size-5 mr-1" />
                                     Nueva hoja de Temperatura
                                     </Link>
                                 </div>
@@ -213,22 +195,14 @@
                                 <div v-if="medicationRecord">
                                     <Link :href="route('medicationRecords.show', medicationRecord)"
                                         class="flex w-full items-center justify-center bg-gradient-to-r from-yellow-500 to-yellow-700 text-white font-semibold rounded-lg p-4 hover:from-yellow-600 hover:to-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
+                                    <MedicationIcon class="size-5 mr-1" />
                                     Ficha de Medicamentos
                                     </Link>
                                 </div>
                                 <div v-else>
                                     <Link :href="route('medicationRecords.create', { admission_id: admission.id })"
                                         class="flex w-full items-center justify-center bg-gradient-to-r from-yellow-500 to-yellow-700 text-white font-semibold rounded-lg p-4 hover:from-yellow-600 hover:to-yellow-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
+                                    <MedicationIcon class="size-5 mr-1" />
                                     Ficha de Medicamentos
                                     </Link>
                                 </div>
@@ -241,13 +215,15 @@
                         <AccessGate :role="['doctor', 'admin']">
                             <div v-if="can.update">
                                 <div v-if="admission.discharged_date == null">
-                                    <PersonalizableButton  @click="admissionUpdateCharge = true" class="gap-2 " color="green" >
+                                    <PersonalizableButton @click="admissionUpdateCharge = true" class="gap-2 "
+                                        color="green">
                                         <CheckCircleIcon class="size-5" />
-                                         <span class="hidden sm:inline-flex">Dar de Alta</span>
+                                        <span class="hidden sm:inline-flex">Dar de Alta</span>
                                     </PersonalizableButton>
                                 </div>
                                 <div v-if="admission.discharged_date != null">
-                                    <PersonalizableButton  @click="admissionBeingPutInProgress = true"  class="gap-2 " color="yellow">
+                                    <PersonalizableButton @click="admissionBeingPutInProgress = true" class="gap-2 "
+                                        color="yellow">
                                         <RestoreIcon class="size-5" />
                                         <span class="hidden sm:inline-flex">Poner en progreso</span>
                                     </PersonalizableButton>
@@ -265,9 +241,10 @@
                             </Link>
                         </AccessGate>
 
-                        <DangerButton v-if="can.delete && admission.active" @click="admissionBeingDeleted = true"  class="gap-2" color="red">
-                           <TrashIcon class="size-5" />
-                              <span class="hidden sm:inline-flex">Eliminar</span>
+                        <DangerButton v-if="can.delete && admission.active" @click="admissionBeingDeleted = true"
+                            class="gap-2" color="red">
+                            <TrashIcon class="size-5" />
+                            <span class="hidden sm:inline-flex">Eliminar</span>
                         </DangerButton>
                         <PersonalizableButton v-if="can.delete && !admission.active" @click="restoreAdmission"
                             class="gap-2" color="green">
@@ -380,9 +357,7 @@
                             class="px-4 py-2 text-sm font-medium rounded-md border border-gray-300 text-gray-700 bg-white hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-700 transition">
                             Cancelar
                         </SecondaryButton>
-                        <PrimaryButton type="submit"
-                            class="px-4 py-2 text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 dark:bg-purple-700 dark:hover:bg-purple-800 transition"
-                            :disabled="!formRecord.receptionist_id">
+                        <PrimaryButton type="submit" :disabled="!formRecord.receptionist_id">
                             Aceptar
                         </PrimaryButton>
                     </div>
@@ -410,6 +385,10 @@ import TrashIcon from '@/Components/Icons/TrashIcon.vue';
 import UserSelector from '@/Components/UserSelector.vue';
 import Modal from '@/Components/Modal.vue';
 import RestoreIcon from '@/Components/Icons/RestoreIcon.vue';
+import MedicalOrderIcon from '@/Components/Icons/MedicalOrderIcon.vue';
+import NurseRecordIcon from '@/Components/Icons/NurseRecordIcon.vue';
+import ChartIcon from '@/Components/Icons/ChartIcon.vue';
+import MedicationIcon from '@/Components/Icons/MedicationIcon.vue';
 import PersonalizableButton from '@/Components/PersonalizableButton.vue';
 import {
     Link
@@ -439,6 +418,10 @@ export default {
         CheckCircleIcon,
         TrashIcon,
         RestoreIcon,
+        MedicationIcon,
+        NurseRecordIcon,
+        ChartIcon,
+        MedicalOrderIcon,
         UserSelector,
         PersonalizableButton,
         AccessGate,

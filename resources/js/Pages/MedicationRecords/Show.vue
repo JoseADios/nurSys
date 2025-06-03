98df8fc54014abd11900d6a6257e1d741933e2b0
EudisContreras <eudiscontreras1329@gmail.com>
2025-06-03 09:43:27 -0400

    fixed: diet name wasn't showing on medication records show edit diet modal

--- resources/js/Pages/MedicationRecords/Show.vue
+++ resources/js/Pages/MedicationRecords/Show.vue
@@ -512,7 +512,7 @@
                                 </label>
                                 <select id="diet-select" v-model="modalform.diet" required
                                     class="text-sm font-medium w-full text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100  p-2.5 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                    <option v-for="diets in diet" :key="diets.id" :value="diets.description">
+                                    <option v-for="diets in diet" :key="diets.id" :value="diets.name">
                                         {{ diets.name }} - {{ diets.description }}
                                     </option>
                                 </select>

720c1625d8a52ce308fc40a90b22f8e9fa836bb9
joseDios <josedios10@gmail.com>
2025-07-06 08:22:50 -0400

    Update medical order policy to restrict create only one record per day

--- app/Policies/MedicalOrderPolicy.php
+++ app/Policies/MedicalOrderPolicy.php
@@ -31,10 +31,10 @@ class MedicalOrderPolicy
      */
     public function view(User $user, MedicalOrder $medicalOrder): Response
     {
-       if ($user->hasRole('doctor')) {
+        if ($user->hasRole('doctor')) {
             return Response::allow();
         }
-        if ( $medicalOrder->doctor_id !== $user->id) {
+        if ($medicalOrder->doctor_id !== $user->id) {
             return Response::deny('No tienes permiso para ver este registro');
         }
 
@@ -44,14 +44,13 @@ class MedicalOrderPolicy
     /**
      * Determine whether the user can create models.
      */
-      //  crear: doctores, en el ingreso solo crear el doctor relacionado,
+    //  crear: doctores, en el ingreso solo crear el doctor relacionado,
     public function create(User $user, $admission_id): Response
     {
         if (!$user->hasRole('doctor')) {
-            return Response::deny('No tienes el rol necesario para crear ordenes medicas');
+            return Response::deny('No tienes el rol necesario para crear ordenes médicas');
         }
 
-        // Log::info($admission_id);
         $admission = Admission::findOrFail($admission_id);
 
         if ($admission->discharged_date !== null) {
@@ -62,9 +61,17 @@ class MedicalOrderPolicy
             return Response::deny('No tienes permiso para crear este registro');
         }
 
+        $orderExistsToday  = MedicalOrder::where('admission_id', $admission_id)
+            ->whereDate('created_at', today())
+            ->exists();
+
+        if ($orderExistsToday ) {
+            return Response::deny('Ya existe una orden médica creada hoy para este ingreso. No se puede crear otra.');
+        }
+
         return Response::allow();
     }
-public function updateAdmission(User $user): Response
+    public function updateAdmission(User $user): Response
     {
         return Response::allow();
     }
@@ -104,21 +111,21 @@ public function updateAdmission(User $user): Response
     public function delete(User $user, MedicalOrder $medicalOrder): Response
     {
         if (!$user->hasRole('doctor')) {
-            return Response::deny('No tienes el rol necesario para crear ordenes medicas');
+            return Response::deny('No tienes el rol necesario para eliminar ordenes medicas');
         }
 
         $admission = $medicalOrder->admission;
 
         if ($admission->discharged_date !== null) {
-            return Response::deny('No se pueden actualizar registros en un ingreso que ya ha sido dado de alta');
+            return Response::deny('No se pueden eliminar registros en un ingreso que ya ha sido dado de alta');
         }
 
         if ($admission->doctor_id !== Auth::id()) {
-            return Response::deny('No tienes permiso para crear este registro');
+            return Response::deny('No tienes permiso para eliminar este registro');
         }
 
         if (!($medicalOrder->created_at >= now()->startOfDay() && $medicalOrder->created_at <= now()->endOfDay())) {
-            return Response::deny('No se pueden actualizar registros de un dia pasado');
+            return Response::deny('No se pueden eliminar registros de un dia pasado');
         }
 
         return Response::allow();

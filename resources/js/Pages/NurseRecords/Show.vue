fcb5dc2e4ffedecfab923e325282ae5028cc0116
joseDios <josedios10@gmail.com>
2025-07-11 20:38:22 -0400

    Fix show nurseRecord withot bed

--- resources/js/Pages/NurseRecords/Show.vue
+++ resources/js/Pages/NurseRecords/Show.vue
@@ -102,7 +102,8 @@
                             class="bg-white dark:bg-gray-800 rounded-lg p-4 border border-gray-200 dark:border-gray-700/60">
                             <h3 class="text-sm font-medium text-gray-500 dark:text-gray-300 mb-2">Sala</h3>
                             <p class="text-lg font-semibold text-gray-900 dark:text-white">
-                                Sala {{ bed.room }}, Cama {{ bed.number }}
+                                Sala {{ nurseRecord.admission.bed?.room || 'N/A' }},
+                                    Cama {{ nurseRecord.admission.bed?.number || 'N/A' }}
                             </p>
                         </div>
 
